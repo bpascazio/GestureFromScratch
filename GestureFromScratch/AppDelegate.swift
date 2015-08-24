98df9830202dd29772fe8f43360d99f64a820f83
Bob Pascazio <bob@bytefly.com>
2015-08-24 19:34:21 -0400

    Setup initial view controller.

--- GestureFromScratch/AppDelegate.swift
+++ GestureFromScratch/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = UIColor.blackColor()
+        
+        let viewController = ViewController()
+        window?.rootViewController = viewController
         return true
     }
 
--- GestureFromScratch/ViewController.swift
+++ GestureFromScratch/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let swipeRec = UISwipeGestureRecognizer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.yellowColor()
+        
+        let viewFrame = self.view.frame
+        
+        let view = UIView(frame:
+            CGRect(x: viewFrame.origin.x, y: viewFrame.origin.y, width: viewFrame.width, height: viewFrame.height))
+        view.backgroundColor = UIColor.blueColor()
+        self.view.addSubview(view)
+        
+        swipeRec.addTarget(self, action: "swipedView")
+        swipeRec.direction = .Down
+        view.addGestureRecognizer(swipeRec)
     }
 
+    func swipedView(){
+        println("view was swiped")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
