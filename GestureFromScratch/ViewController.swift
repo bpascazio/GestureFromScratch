863b6e43b2291bcae088eee4281510cf59bf087f
Bob Pascazio <bob@bytefly.com>
2015-08-24 19:10:02 -0400

    Initial Commit

---
+++ GestureFromScratch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GestureFromScratch
+//
+//  Created by Bob Pascazio on 8/24/15.
+//  Copyright (c) 2015 Bob Pascazio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
